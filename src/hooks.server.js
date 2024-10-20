616a9afbb0b479dda62d1b7be08d235e06ae8a11
Jake Owen <jakeowen.ex@gmail.com>
2024-10-20 18:55:21 +0100

    adsa

--- src/hooks.server.js
+++
@@ -1,9 +0,0 @@
-export async function handle({ event, resolve }) {
-	const response = await resolve(event);
-
-	// Add the CSP header
-	response.headers.set('Content-Security-Policy', "script-src 'self' https://www.vercel-insights.com");
-	response.headers.set('Access-Control-Allow-Origin', '*');
-
-	return response;
-}

186771af28bc81914607726baaf13f51cb124474
Jake Owen <jakeowen.ex@gmail.com>
2024-10-20 19:02:23 +0100

    adsa

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -2,7 +2,7 @@ export async function handle({ event, resolve }) {
 	const response = await resolve(event);
 
 	// Add the CSP header
-	response.headers.set('Content-Security-Policy', "script-src 'self' unsafe-inline https://www.vercel-insights.com https://vercel.live");
+	response.headers.set('Content-Security-Policy', "script-src 'self' 'unsafe-inline' https://www.vercel-insights.com https://vercel.live");
 	response.headers.set('Access-Control-Allow-Origin', '*');
 
 	return response;

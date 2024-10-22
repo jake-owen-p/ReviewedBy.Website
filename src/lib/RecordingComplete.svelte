0ad53f34790976c9e631edab1085def1eec065b5
Jake Owen <jakeowen.ex@gmail.com>
2024-10-22 12:42:31 +0100

    asd

--- src/lib/RecordingComplete.svelte
+++ src/lib/RecordingComplete.svelte
@@ -3,22 +3,35 @@
 	export let playRecording;
 	export let onSave;
 	export let savingVideoState;
+	import { page } from '$app/stores';
 
-	$: console.log(savingVideoState)
+	let checked = false;
+	let email = $page.url.searchParams.get('email');
 </script>
 
 <div class="container">
 	<div class="text_container">
 		<p class="text">Recording Complete</p>
 		{#if savingVideoState === 'saved'}
-			<p class="subtext">Thank you for completing the interview, one of our experts will provide feedback via email within 24 hours </p>
+			<p class="subtext">Thank you for completing the interview, one of our professionals will provide feedback via email within 24 hours </p>
 		{:else}
 			<p class="subtext">Please submit your interview to be reviewed, or you can start again if you want to give it another go!</p>
-			{#if savingVideoState === 'saving'}
-				<img alt="spinner" src="/spinner.svg" />
-			{:else}
-				<button class="button" on:click={onSave}>Submit for Review</button>
-			{/if}
+			<div>
+				<div style="padding: 0 20px">
+					<h6 class="heading">Email</h6>
+					<input class="email_input" bind:value={email} />
+				</div>
+				<div style="display: flex; padding: 0 20px; align-items: flex-start; margin-top: 15px">
+					<input type="checkbox" bind:checked={checked} />
+					<p style="text-align: left; margin-left: 10px">I agree to the Terms of Use and Privacy Policy.</p>
+				</div>
+				<p></p>
+				{#if savingVideoState === 'saving'}
+					<img style="margin-top: 10px; width: 30px; height: 30px" alt="spinner" src="/grads/spinner.svg" />
+				{:else}
+					<button class="button" on:click={() => onSave(email)} class:disabled={!checked || !email || email?.length < 1}>Submit for Review</button>
+				{/if}
+			</div>
 	  {/if}
 	</div>
 	<div class="buttons">
@@ -38,6 +51,23 @@
 </div>
 
 <style>
+		.heading {
+				margin: 0;
+        text-align: left;
+				font-size: 16px;
+		}
+    .email_input {
+				margin-top: 10px;
+        width: 100%;
+        height: 30px;
+        border-radius: 8px;
+        padding: 0 10px;
+        font-size: 14px;
+        border: 1px solid rgba(47, 72, 88, 0.3);
+    }
+    .email_input:focus {
+        outline: none;
+    }
 		.text_container {
 				text-align: center;
 				padding: 10px 20px 15px 20px;
@@ -81,16 +111,23 @@
     }
     .subtext {
         font-size: 16px;
-				margin-top: 20px;
-				margin-bottom: 20px;
+        background-color: #eff6ff;
+				border-radius: 10px;
+				padding: 10px;
+        margin: 10px 20px;
     }
+		.disabled {
+        background-color: #717171 !important;
+		}
     .button {
         background-color: var(--primary);
+				border: none;
 				color: white;
 				font-weight: bold;
-				padding: 7px 20px;
+				padding: 8px 22px;
 				border-radius: 10px;
 				font-size: 16px;
 				cursor: pointer;
+				margin-top: 10px;
     }
 </style>

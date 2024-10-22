eac96e1c9368ccb1f9eb30fa560ce8f4d012b7ee
Jake Owen <jakeowen.ex@gmail.com>
2024-10-22 21:21:14 +0100

    test

--- src/lib/questions.ts
+++ src/lib/questions.ts
@@ -2,4 +2,5 @@ export const Questions = {
 	general: "Can you share a time where you had to overcome a challenging situation?",
 	engineer_general: "What is your favourite feature from any programming language?",
 	engineer_bug: "What’s a challenging bug you’ve encountered, and how did you solve it?",
+	engineer_learn: "Can you describe a project where you had to learn a new technology or tool? How did you approach it?",
 }
